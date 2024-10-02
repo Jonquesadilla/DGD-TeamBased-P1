aea7d67feaf9a1de2b33fed8d2f7d8eb3dc9831c
Jonquesadilla <149528069+Jonquesadilla@users.noreply.github.com>
2024-10-01 22:48:41 -0400

    Audio stuff

--- Assets/Audio Stuff/Sound Effects/LongHonkTimer.cs
+++ Assets/Audio Stuff/Sound Effects/LongHonkTimer.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AmbulanceSoundsTimer : MonoBehaviour
+public class LongHonkTimer : MonoBehaviour
 {
     
     public AudioSource AS;
     public AudioClip AC;
 
-    public float timer = 10f;
+    public float timer = 7f;
 
-    public float min = 20;
+    public float min = 5;
 
-    public float max = 30;
+    public float max = 15;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Audio Stuff/Sound Effects/ShortHonkTimer.cs
+++ Assets/Audio Stuff/Sound Effects/ShortHonkTimer.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AmbulanceSoundsTimer : MonoBehaviour
+public class ShortHonkTimer : MonoBehaviour
 {
     
     public AudioSource AS;
     public AudioClip AC;
 
-    public float timer = 10f;
+    public float timer = 5f;
 
-    public float min = 20;
+    public float min = 10;
 
-    public float max = 30;
+    public float max = 20;
     // Start is called before the first frame update
     void Start()
     {
