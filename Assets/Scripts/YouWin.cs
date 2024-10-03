7ed4c693f9a2aa6654c311563e691baaef1cbc90
TyChi69 <tylerchau53@gmail.com>
2024-10-03 11:43:21 -0400

    everything should be working

---
+++ Assets/Scripts/YouWin.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class YouWin : MonoBehaviour
+
+{
+    public static float HighScore;
+    public TextMeshProUGUI Highscore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Highscore.text = "Best time:"+ HighScore;
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene("Main Scene");
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKey(KeyCode.M))
+        {
+            SceneManager.LoadScene("Title Screen");
+        }
+    }
+}
